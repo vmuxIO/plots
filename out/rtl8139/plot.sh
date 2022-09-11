3c40e19d3bdc8f2644d96c61cdcc4cb3b42a0807
Sandro-Alessio Gierens <sandro@gierens.de>
2022-09-12 00:52:07 +0200

    Add top level plot script for rtl8139

---
+++ out/rtl8139/plot.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+m='pcvm'
+for n in 'bridge' 'macvtap'; do
+    q='rtl8139'
+    for v in 'vhostoff' 'vhoston'; do
+        for i in 'ioregionfdoff' 'ioregionfdon'; do
+            r='xdp'
+            for s in "60B" "1020B"; do
+                name="$m $n $q $v $i $r $s"
+                infix1="${m}_${n}_${q}_${v}_${i}_${r}"
+                infix2="$s"
+                infix="${infix1}_${infix2}"
+
+                echo "Plotting ${name}"
+                # ls "../../dat/rtl8139/acc_histogram_${infix1}_"*"_${infix2}_"*
+                # ls "../../dat/rtl8139/output_${infix1}_"*"_${infix2}_"*
+
+                ../../plot_load_latency.py \
+                    --red "../../dat/rtl8139/acc_histogram_${infix1}_"*"_${infix2}_"* \
+                    --red-name "" \
+                    --logarithmic \
+                    --width 6 \
+                    --height 6 \
+                    --output "load_latency_${infix}.pdf"
+
+                ../../plot_packet_loss.py \
+                    --red "../../dat/rtl8139/output_${infix1}_"*"_${infix2}_"* \
+                    --red-name "" \
+                    --width 6 \
+                    --height 6 \
+                    --output "packet_loss_${infix}.pdf"
+            done
+        done
+    done
+done

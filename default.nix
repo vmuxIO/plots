58015de8c4aac5c5258bbccd763636f6a66464a3
Pogobanane <aenderboy@gmx.de>
2023-08-23 12:45:51 +0200

    add nix-shell

---
+++ default.nix
@@ -0,0 +1,12 @@
+{pkgs ? import <nixpkgs> {} }:
+  pkgs.mkShell {
+    buildInputs = with pkgs.python3.pkgs; [
+      black # auto formatting
+      flake8 # annoying "good practice" annotations
+      mypy # static typing
+      pkgs.ruff # language server ("linting")
+
+      numpy
+      matplotlib
+    ];
+  }

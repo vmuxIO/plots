31a8c5c38cb309b572e77d82c563b2cd4ffb88b7
Pogobanane <aenderboy@gmx.de>
2024-06-27 20:27:35 +0200

    restructure plot_mediation formatting

--- plot_mediation.py
+++ plot_mediation.py
@@ -97,20 +97,35 @@ def parse_args(parser):
 
     return args
 
+hatches = ['O', '\\\\', '*', 'o', 'xx', '.O', '//', '..']
+# hatches = ['/', '\\', '|', '-', '+', 'x', 'o', 'O']
+hatches_used = 0
+
+# Define a custom function to add hatches to the bar plots
+def barplot_with_hatches(*args, **kwargs):
+    global hatches_used
+    sns.barplot(*args, **kwargs)
+    for i, bar in enumerate(plt.gca().patches):
+        hatch = hatches[hatches_used % len(hatches)]
+        print(hatch)
+        bar.set_hatch(hatch)
+        hatches_used += 1
+
 
 def main():
     parser = setup_parser()
     args = parse_args(parser)
 
-    fig = plt.figure(figsize=(args.width, args.height))
-    ax = fig.add_subplot(1, 1, 1)
-    ax.set_axisbelow(True)
+    # fig = plt.figure(figsize=(args.width, args.height))
+    # fig = plt.figure()
+    # ax = fig.add_subplot(1, 1, 1)
+    # ax.set_axisbelow(True)
     if args.title:
         plt.title(args.title)
     plt.grid()
     # plt.xlim(0, 0.83)
     log_scale = (False, True) if args.logarithmic else False
-    ax.set_yscale('log' if args.logarithmic else 'linear')
+    # ax.set_yscale('log' if args.logarithmic else 'linear')
 
     dfs = []
     for color in COLORS:
@@ -132,38 +147,74 @@ def main():
     summary = df.groupby(hue)['rxMppsCalc'].describe()
     df_hue = df.apply(lambda row: '_'.join(str(row[col]) for col in ['repetitions', 'interface', 'fastclick', 'rate']), axis=1)
     df_hue = map_hue(df_hue, hue_map)
+    df['is_passthrough'] = df.apply(lambda row: True if "vmux-pt" in row['interface'] or "vfio" in row['interface'] else False, axis=1)
+
+    # map colors to hues
+    colors = sns.color_palette("pastel", len(df['hue'].unique()))
+    palette = dict(zip(df['hue'].unique(), colors))
 
     # Plot using Seaborn
-    bar = sns.barplot(x='num_vms', y='rxMppsCalc', hue="hue", data=pd.concat(dfs),
-                palette='colorblind',
-                edgecolor='dimgray',
-                # kind='bar',
-                # capsize=.05,  # errorbar='sd'
-                # log_scale=log_scale,
-                ax=ax,
-                )
+    grid = sns.FacetGrid(df,
+            col='is_passthrough',
+            sharey = False,
+    )
+    grid.map_dataframe(sns.barplot,
+    # grid.map_dataframe(barplot_with_hatches,
+                       x='num_vms',
+                       y='rxMppsCalc',
+                       hue="hue",
+                       palette=palette,
+                       edgecolor="dimgray",
+                       )
+
+    grid.add_legend(bbox_to_anchor=(0.5, 0.77), loc='center', ncol=1)
+
+    # Fix the legend hatches
+    for i, legend_patch in enumerate(grid._legend.get_patches()):
+        hatch = hatches[i % len(hatches)]
+        legend_patch.set_hatch(f"{hatch}{hatch}")
+    # add hatches to bars
+    hatches_used = 0
+    for (i, j, k), data in grid.facet_data():
+        for bar in grid.facet_axis(i, j).patches:
+            if bar.get_bbox().x0 == 0 and bar.get_bbox().x1 == 0 and bar.get_bbox().y0 == 0 and bar.get_bbox().y1 == 0:
+                # skip bars that are not rendered
+                continue
+            hatch = hatches[hatches_used % len(hatches)]
+            print(bar, hatch)
+            bar.set_hatch(hatch)
+            hatches_used += 1
+
+    grid.figure.set_size_inches(args.width, args.height)
+    grid.set_titles("")
+    plt.subplots_adjust(top=0.5, left=0.2, right=0.95)
+    # bar = sns.barplot(x='num_vms', y='rxMppsCalc', hue="hue", data=pd.concat(dfs),
+    #             palette='colorblind',
+    #             edgecolor='dimgray',
+    #             # kind='bar',
+    #             # capsize=.05,  # errorbar='sd'
+    #             # log_scale=log_scale,
+    #             ax=ax,
+    #             )
     # sns.move_legend(
     #     ax, "lower center",
     #     bbox_to_anchor=(.5, 1), ncol=3, title=None, frameon=False,
     # )
     #
-    sns.move_legend(
-        ax, "lower center",
-        bbox_to_anchor=(0.45, 1),
-        ncol=1,
-        title=None,
-        # frameon=False,
-    )
-    plt.xlabel(XLABEL)
-    plt.ylabel(YLABEL)
+    # sns.move_legend(
+    #     grid, "lower center",
+    #     bbox_to_anchor=(0.45, 1),
+    #     ncol=1,
+    #     title=None,
+    #     # frameon=False,
+    # )
+    grid.set_xlabels(XLABEL)
+    grid.set_ylabels(YLABEL)
     # plt.ylim(0, 1)
     if not args.logarithmic:
         plt.ylim(bottom=0)
     # for container in ax.containers:
     #     ax.bar_label(container, fmt='%.0f')
-
-    # Define some hatches
-    hatches = ['-', '+', 'x', '\\', '*', 'o', '.O', '//']
 
     # # iterate through each container, hatch, and legend handle
     # for container, hatch, handle in zip(ax.containers, hatches, ax.get_legend().legend_handles[::-1]):
@@ -183,8 +234,8 @@ def main():
     # legend.get_frame().set_facecolor('white')
     # legend.get_frame().set_alpha(0.8)
     # fig.tight_layout(rect = (0, 0, 0, 0.1))
-    ax.set_position((0.1, 0.1, 0.5, 0.8))
-    fig.tight_layout()
+    # ax.set_position((0.1, 0.1, 0.5, 0.8))
+    # fig.tight_layout()
     # fig.tight_layout(rect=(0, 0, 0.3, 1))
     plt.savefig(args.output.name)
     plt.close()

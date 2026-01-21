2b3af64c6e47847905281a46d863f0b43e251849
Pogobanane <aenderboy@gmx.de>
2026-01-22 00:44:12 +0100

    fixup deps

--- default.nix
+++ default.nix
@@ -1,7 +1,11 @@
 {pkgs ? import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {} }:
   pkgs.mkShell {
-    buildInputs = [
-      pkgs.marimo
+    buildInputs = with pkgs; [
+      marimo
+      cargo
+      gdb
+      cargo-watch
+      rust-analyzer
   ] ++ (with pkgs.python3.pkgs; [
       black # auto formatting
       flake8 # annoying "good practice" annotations
@@ -13,6 +17,8 @@
       seaborn
       tqdm
       scipy
+      pandas
+      pyarrow
       #   (ortools.overrideAttrs (final: prev: {
       #    buildInputs = prev.buildInputs ++ [ pkgs.scipopt-scip ];
       #    propagatedBuildInputs = prev.propagatedBuildInputs ++ [ pkgs.scipopt-scip ];

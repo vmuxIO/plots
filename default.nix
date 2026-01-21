f61650466c645d13e0a49cca0d263fb7f446d806
Pogobanane <aenderboy@gmx.de>
2026-01-21 13:36:50 +0100

    disable broken solvers

--- default.nix
+++ default.nix
@@ -1,6 +1,8 @@
 {pkgs ? import (fetchTarball "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz") {} }:
   pkgs.mkShell {
-    buildInputs = with pkgs.python3.pkgs; [
+    buildInputs = [
+      pkgs.marimo
+  ] ++ (with pkgs.python3.pkgs; [
       black # auto formatting
       flake8 # annoying "good practice" annotations
       mypy # static typing
@@ -11,11 +13,11 @@
       seaborn
       tqdm
       scipy
-      (ortools.overrideAttrs (final: prev: {
-        buildInputs = prev.buildInputs ++ [ pkgs.scipopt-scip ];
-        propagatedBuildInputs = prev.propagatedBuildInputs ++ [ pkgs.scipopt-scip ];
-        cmakeFlags = prev.cmakeFlags ++ [ (lib.cmakeBool "USE_SCIP" true) ];
-        NIX_CFLAGS_COMPILE = [ "-Wno-format-security" ];
-      }))
-    ];
+      #   (ortools.overrideAttrs (final: prev: {
+      #    buildInputs = prev.buildInputs ++ [ pkgs.scipopt-scip ];
+      #    propagatedBuildInputs = prev.propagatedBuildInputs ++ [ pkgs.scipopt-scip ];
+      #    cmakeFlags = prev.cmakeFlags ++ [ (lib.cmakeBool "USE_SCIP" true) ];
+      #    NIX_CFLAGS_COMPILE = [ "-Wno-format-security" ];
+      #  }))
+    ]);
   }
